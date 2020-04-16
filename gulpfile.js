67523ce97ebe2fc16dd846fe86597ed2d1f709a3
Elf-mousE <ifmouz@gmail.com>
2020-04-16 15:54:33 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 /* eslint-env node */
 const balm = require('balm');
 const config = require('./config/balmrc');
-const publish = require('./config/publish');
 
 balm.config = config;
 
 balm.go(mix => {
-  publish(mix);
-
   if (mix.env.isDev) {
     mix.copy('node_modules/font-awesome/fonts/*', 'app/fonts');
   }
